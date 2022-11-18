d81d01071b77a68ed321a275a85f1b307a783d93
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-11-19 00:59:29 +0800

    feat: Initialize Project

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route('/home')
+def home():
+  return render_template('home.html')
+
+if __name__ == "__main__":
+  app.run() 
