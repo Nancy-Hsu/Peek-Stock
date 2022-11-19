24a8196e7f28104c72e2facd42530a4a946d7454
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-11-19 21:18:53 +0800

    feat:  add scraper.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template
-import requests
-from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+from scraper import getStock
+
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
-@app.route('/home')
-def home():
-  return render_template('home.html')
+@app.route("/stock")
+def stock():
+  stock_urls = [
+      'https://tw.stock.yahoo.com/quote/2330',
+      'https://tw.stock.yahoo.com/quote/2330',
+      'https://tw.stock.yahoo.com/quote/2317',
+      'https://tw.stock.yahoo.com/quote/6547'
+  ]
+  executor = ThreadPoolExecutor()
+  with ThreadPoolExecutor() as executor:
+    data = list(executor.map(getStock, stock_urls))
+  
+  
+  return render_template('home.html', data = data)
 
 if __name__ == "__main__":
-  app.run() 
+  app.run(debug=True)
